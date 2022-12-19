44cb70dc63e3599e003bd2857dde8610819973b3
Farid Haziyev <fhaziyev@omilia.com>
2022-12-19 11:38:23 +0400

    made ui cleaner. Added a second button, just for typing

--- src/components/VoiceFooter.vue
+++ src/components/VoiceFooter.vue
@@ -1,158 +1,159 @@
-<template>
-	<!-- Footer: Input field + button -->
-	<footer>
-		<audio class="sound-clips"></audio>
-		<canvas id="sound-visualizer" width="100%" height="300px">
-		</canvas>
-		<transition name="fade" mode="out-in">
-			<div v-if="chatStart && !isRecording">
-				<button class="start-button btn" :disabled="disableRecording" @click="startRecord()">
-					<font-awesome-icon icon="microphone" class="micro-icon"/>
-				</button>
-			</div>
-		</transition>
-		<transition name="fade" mode="out-in">
-			<div v-if="!chatStart">
-				<button class="start-button btn" @click="startChat()">START</button>
-			</div>
-		</transition>
-    <transition name="fade" mode="out-in">
-			<div v-if="chatStart">
-				<input type="user_email" name="user_email" v-model="user_email"/>
-        <button @click="sendEmailtoBot()">Submit</button>
-			</div>
-		</transition>
-		<transition name="fade" mode="out-in">
-			<div v-if="chatStart && isRecording">
-				<button class="start-button btn" @click="stopRecording()">PAUSE</button>
-			</div>
-		</transition>
-	</footer>
-</template>
+  <template>
+    <!-- Footer: Input field + button -->
+    <footer>
+      <audio class="sound-clips"></audio>
+      <canvas id="sound-visualizer" width="100%" height="300px">
+      </canvas>
+      <transition name="fade" mode="out-in">
+        <div v-if="chatStart && !isRecording">
+          <button class="start-button btn" :disabled="disableRecording" @click="startRecord()">
+            <font-awesome-icon icon="microphone" class="micro-icon"/>
+          </button>
+          <button class="start-typing btn" @click="startTyping()">
+           <font-awesome-icon icon="keyboard" class="micro-icon"/>
+          </button>
+        </div>
+      </transition>
+      <transition name="fade" mode="out-in">
+        <div v-if="!chatStart">
+          <button class="start-button btn" @click="startChat()">START</button>
+        </div>
+      </transition>
+      <transition name="fade" mode="out-in">
+        <div v-if="chatStart && isRecording">
+          <button class="start-button btn" @click="stopRecording()">PAUSE</button>
+        </div>
+      </transition>
+    </footer>
+  </template>
 
-<script>
-/* eslint-disable no-unused-vars */
-/* Helpers */
-import { mapGetters, mapState } from 'vuex';
-/* Mixins */
-import MessageHandlerMixin from '../mixins/MessageHandlerMixin';
+  <script>
+  /* eslint-disable no-unused-vars */
+  /* Helpers */
+  import { mapGetters, mapState } from 'vuex';
+  /* Mixins */
+  import MessageHandlerMixin from '../mixins/MessageHandlerMixin';
 
-import '@/assets/lib/vad';
+  import '@/assets/lib/vad';
 
-export default {
-	name: 'Footer',
-	mixins: [MessageHandlerMixin],
-	data() {
-		return {
-      user_email: '',
-			audioContext: undefined,
-			mediaRecorder: {},
-			isRecording: false,
-			dataArray: [],
-			analyser: undefined,
-			canvas: undefined,
-			ctx: undefined,
-			canvasHeight: undefined,
-			canvasWidth: undefined,
-			countFrame: 0,
-			recordTime: undefined
-		};
-	},
-	computed: {
-		...mapGetters(['chatStart', 'inputVisible', 'currentMessage', 'waitForResponse']),
-		...mapState(['incomeMessage', 'disableRecording']),
-		readyToRecord() {
-			console.log('computed');
-			if (this.waitForRespose && this.incomeMessage.length === 1) { return true; }
-			return false;
-		}
-	},
-	// watch: {
-	// 	waitForResponse(value) {
-	// 		console.log('watch');
-	// 		console.log(value);
-	// 		console.log(this.currentMessage.in.length);
-	// 		if (!value && this.currentMessage.in.length === 1) {
-	// 			this.mediaRecorder.start();
-	// 			this.isRecording = true;
-	// 		}
-	// 	}
-	// },
-	mounted() {
-		// init canvas
-		this.canvas = document.getElementById('sound-visualizer');
-		this.ctx = this.canvas.getContext('2d');
-		this.canvasHeight = this.canvas.scrollHeight;
-		this.canvasWidth = this.canvas.scrollWidth;
-	},
-	methods: {
-    sendEmailtoBot(){
-      this.sendMessageToBot(this.user_email)
+  export default {
+    name: 'Footer',
+    mixins: [MessageHandlerMixin],
+    data() {
+      return {
+        user_email: '',
+        audioContext: undefined,
+        mediaRecorder: {},
+        isRecording: false,
+        can_type: false,
+        dataArray: [],
+        analyser: undefined,
+        canvas: undefined,
+        ctx: undefined,
+        canvasHeight: undefined,
+        canvasWidth: undefined,
+        countFrame: 0,
+        recordTime: undefined
+      };
     },
-		startChat() {
-			this.$store.commit('setChatStart', true);
-			// this.startRecord();
-		},
-		startRecord() {
-			const vm = this;
-			window.AudioContext = window.AudioContext || window.webkitAudioContext;
-			this.audioContext = new AudioContext({ sampleRate: 16000 });
-			this.isRecording = true;
-			// Ask for audio device
-			navigator.getUserMedia = navigator.getUserMedia
-				|| navigator.mozGetUserMedia
-				|| navigator.webkitGetUserMedia;
-			navigator.getUserMedia({ audio: true }, vm.startUserMedia, e => {
-				console.log(`No live audio input in this browser: ${e}`);
-			});
-		},
-		stopRecording() {
-			this.isRecording = false;
-			this.audioContext.close().then(() => {
-				console.log('streaming close');
-			});
-		},
-		startUserMedia(stream) {
-			const vm = this;
-			vm.analyser = vm.audioContext.createAnalyser();
-			const source = vm.audioContext.createMediaStreamSource(stream).connect(vm.analyser);
-			console.log(vm.analyser);
-			vm.dataArray = new Uint8Array(vm.analyser.fftSize / 32);
-			vm.animate();
-			const chunks = [];
-			const audioOptions = {
-				audioBitsPerSecond: 16000
-			};
-			/*         This two lines using Mediarecorder            */
-			// vm.mediaRecorder = new MediaRecorder(stream, audioOptions);
-			// vm.mediaRecorder.start();
-			vm.mediaRecorder = new Recorder(source, { numChannels: 1 });
-			vm.mediaRecorder.record();
-			window.setTimeout(() => {
-				if (vm.mediaRecorder.state === 'recording') {
-					vm.mediaRecorder.stop();
-					console.log('10 seconds, stop recording');
-				}
-			}, 10000);
-			// Setup options
-			const options = {
-				source: source,
-				voice_stop: function() {
-					/*         This line using Mediarecorder            */
-					// if (vm.mediaRecorder.state === 'recording') {
-					if (vm.isRecording) {
-						vm.mediaRecorder.stop();
-						vm.mediaRecorder.exportWAV(vm.createFileLink);
-						vm.stopRecording();
-						console.log('voice_stop');
-					}
-				},
-				voice_start: function() {
-					if (vm.isRecording) {
-						console.log('voice_start');
-					// } else if (!vm.isRecording && (this.incomeMessage.length === 0)) {
-					// 	console.log('StartRecording');
-					// 	vm.startRecord();
+    computed: {
+      ...mapGetters(['chatStart', 'inputVisible', 'currentMessage', 'waitForResponse']),
+      ...mapState(['incomeMessage', 'disableRecording']),
+      readyToRecord() {
+        console.log('computed');
+        if (this.waitForRespose && this.incomeMessage.length === 1) { return true; }
+        return false;
+      }
+    },
+    // watch: {
+    // 	waitForResponse(value) {
+    // 		console.log('watch');
+    // 		console.log(value);
+    // 		console.log(this.currentMessage.in.length);
+    // 		if (!value && this.currentMessage.in.length === 1) {
+    // 			this.mediaRecorder.start();
+    // 			this.isRecording = true;
+    // 		}
+    // 	}
+    // },
+    mounted() {
+      // init canvas
+      this.canvas = document.getElementById('sound-visualizer');
+      this.ctx = this.canvas.getContext('2d');
+      this.canvasHeight = this.canvas.scrollHeight;
+      this.canvasWidth = this.canvas.scrollWidth;
+    },
+    methods: {
+      startChat() {
+        this.$store.commit('setChatStart', true);
+        // this.startRecord();
+      },
+      startTyping()
+      {
+        this.user_email = prompt("enter_text") 
+        this.sendMessageToBot(this.user_email) 
+      },
+      
+      startRecord() {
+        const vm = this;
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        this.audioContext = new AudioContext({ sampleRate: 16000 });
+        this.isRecording = true;
+        // Ask for audio device
+        navigator.getUserMedia = navigator.getUserMedia
+          || navigator.mozGetUserMedia
+          || navigator.webkitGetUserMedia;
+        navigator.getUserMedia({ audio: true }, vm.startUserMedia, e => {
+          console.log(`No live audio input in this browser: ${e}`);
+        });
+      },
+      stopRecording() {
+        this.isRecording = false;
+        this.audioContext.close().then(() => {
+          console.log('streaming close');
+        });
+      },
+      startUserMedia(stream) {
+        const vm = this;
+        vm.analyser = vm.audioContext.createAnalyser();
+        const source = vm.audioContext.createMediaStreamSource(stream).connect(vm.analyser);
+        console.log(vm.analyser);
+        vm.dataArray = new Uint8Array(vm.analyser.fftSize / 32);
+        vm.animate();
+        const chunks = [];
+        const audioOptions = {
+          audioBitsPerSecond: 16000
+        };
+        /*         This two lines using Mediarecorder            */
+        // vm.mediaRecorder = new MediaRecorder(stream, audioOptions);
+        // vm.mediaRecorder.start();
+        vm.mediaRecorder = new Recorder(source, { numChannels: 1 });
+        vm.mediaRecorder.record();
+        window.setTimeout(() => {
+          if (vm.mediaRecorder.state === 'recording') {
+            vm.mediaRecorder.stop();
+            console.log('10 seconds, stop recording');
+          }
+        }, 5000);
+        // Setup options
+        const options = {
+          source: source,
+          voice_stop: function() {
+            /*         This line using Mediarecorder            */
+            // if (vm.mediaRecorder.state === 'recording') {
+            if (vm.isRecording) {
+              vm.mediaRecorder.stop();
+              vm.mediaRecorder.exportWAV(vm.createFileLink);
+              vm.stopRecording();
+              console.log('voice_stop');
+            }
+          },
+          voice_start: function() {
+            if (vm.isRecording) {
+              console.log('voice_start');
+            // } else if (!vm.isRecording && (this.incomeMessage.length === 0)) {
+            // 	console.log('StartRecording');
+            // 	vm.startRecord();
 					}
 				}
 			};
@@ -273,6 +274,24 @@ export default {
 		width: calc(100% - 30px);
 		border-radius: 32px;
 	}
+  .start-typing {
+    font-size: 11pt;
+    color: #fff;
+    position: absolute;
+    top: -37.5px;
+    left: calc(50% + 80.0px);
+    width: 75px;
+    height: 75px;
+    border-radius: 37.5px;
+    border: none;
+    box-shadow: -10px -10px 15px rgba(123, 1, 188, 1) inset,
+      -1px -1px 15px rgba(0, 0, 0, 0.8);
+    background: rgb(187,133,216);
+    background: -moz-linear-gradient(151deg, rgba(187,133,216,1) 0%, rgba(123,1,188,1) 89%);
+    background: -webkit-linear-gradient(151deg, rgba(187,133,216,1) 0%, rgba(123,1,188,1) 89%);
+    background: linear-gradient(151deg, rgba(187,133,216,1) 0%, rgba(123,1,188,1) 89%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#bb85d8", endColorstr="#7b01bc", GradientType=1);
+  }
 	.start-button {
 		font-size: 11pt;
 		color: #fff;
